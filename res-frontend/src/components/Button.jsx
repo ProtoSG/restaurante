6ff6ce47a18f1c6a061631d57dd958efbab83d8c
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-03-02 12:59:54 -0500

    gh-pages added

--- res-frontend/src/components/Button.jsx
+++ res-frontend/src/components/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ name, onClick, bgColor}) {
 
         <button
             onClick={onClick}
-            className={`bg-${bgColor ? 'accept-100' : 'primary-100'} w-full py-4 text-accent-200 rounded-3xl hover:bg-primary-200 hover:scale-105 transition-all hover:font-bold text-2xl`}>
+            className={` bg-${bgColor ? '[#FF5733]' : 'primary-100'} w-full py-4 text-accent-200 rounded-3xl hover:bg-primary-200 hover:scale-105 transition-all hover:font-bold text-2xl`}>
             {name}
         </button>
     )
